8aed95b5e9934b0ddb70db0119683e08949df691
Jeremy André <jeremy@agentik.net>
2026-07-18 22:39:27 +0200

    feat(desktop): WP0 ported design-kit theme foundation (KitProvider + tokens)

--- packages/desktop/src/main.tsx
+++ packages/desktop/src/main.tsx
@@ -1,9 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from "react"
+import ReactDOM from "react-dom/client"
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+import "./theme/globals.css"
+import App from "./App"
+import { KitProvider } from "./theme/KitProvider"
+
+const rootEl = document.getElementById("root")
+if (!rootEl) throw new Error("#root element missing from index.html")
+
+ReactDOM.createRoot(rootEl).render(
   <React.StrictMode>
-    <App />
+    <KitProvider>
+      <App />
+    </KitProvider>
   </React.StrictMode>,
-);
+)
